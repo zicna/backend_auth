bb0a368e05acd8583c1527a037b93c41cf11d19c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-01-20 15:23:10 -0600

    add create method to UserController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::UsersController < ApplicationController
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: { user: UserSerializer.new(@user) }, status: :created
+        else
+            render json: { error: 'faild to create user' }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :bio, :avatar)
+    end
 end
