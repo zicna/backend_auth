bf9eccf48d57e8fb610c52d2e1e2b512a7e1a3f4
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-01-20 15:14:14 -0600

    create custom 'authenticate method'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :username, uniqueness: { case_sensitive: false}
+
+
+    def authenticate(plaintext_password)
+        if BCrypt::Password.new(self.password_digest) == plaintext_password
+            self
+        else
+            false
+        end
+    end
 end
