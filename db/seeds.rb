b8f3393e107c16c370fb702bed7a2dadbcd5e349
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-01-20 15:13:52 -0600

    add one user to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# * for BCrypt::Password.new....
+userOne = User.create(username: 'userOne', password: '1234', bio: 'bio', avatar: 'avatar')
