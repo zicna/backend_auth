d94ad87734d95b0638f6e5d047f0ff6850c365a6
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-01-21 11:22:38 -0600

    lock app with authorized but unlock create new user option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+before_action :authorized
+
     def encode_token(payload)
         JWT.encode(payload, 'my_s3cr3t')
     end
@@ -29,5 +31,9 @@ class ApplicationController < ActionController::API
 
     def logged_in?
         !!current_user
+    end
+
+    def authorized
+        render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
     end
 end
