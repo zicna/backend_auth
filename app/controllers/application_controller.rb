19a6c125b472a1c1f1dd546f2ae2608d0a65b819
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-01-21 11:13:11 -0600

    add funtionality into ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+    def encode_token(payload)
+        JWT.encode(payload, 'my_s3cr3t')
+    end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decode_token(token)
+
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'my_s3cr3t', true, alogorithm: 'HS256' )
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
 end
